f7d4953dabf2fe32259ee1dbb7817eb39eba7150
Robert Froeschl <rwfroeschl@gmail.com>
2024-10-11 16:44:56 -0500

    finished the pics project

---
+++ pics/src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImages = async (term) => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    headers: {
+      Authorization: "Client-ID cJxXBEvfSYpegeq2LEzb4qntic29r-TNYfrniFMxnZo",
+    },
+    params: {
+      query: term,
+    },
+  });
+  console.log(response.data.results);
+  return response.data.results;
+};
+
+export default searchImages;
